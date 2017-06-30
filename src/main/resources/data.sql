e45c8437122745644a1fb13576367930d3b03a45
f4789334 <manmay.e.mohanty@gmail.com>
2017-06-30 17:38:54 +0200

    Fixing data and properties

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,7 +7,7 @@ VALUES (35,  'Shanil', 'Bhowani', 'manmay.mohanty@reverside.co.za', '0846860904'
 INSERT INTO employees (id, name, surname, email, mobile, dob, doj)
 VALUES (74,  'Elvis', 'Phahlamohlaka', 'manmay.mohanty@reverside.co.za', '0846860904',  '16/02', '01/04/2009');
 INSERT INTO employees (id, name, surname, email, mobile, dob, doj)
-VALUES (87,  'Cressundra', 'Alves', 'manmay.mohanty@reverside.co.za', '0846860904',  '27/06', '27/06/2008');
+VALUES (87,  'Cressundra', 'Alves', 'manmay.mohanty@reverside.co.za', '0846860904',  '30/06', '27/06/2008');
 INSERT INTO employees (id, name, surname, email, mobile, dob, doj)
 VALUES (88,  'Martha', 'Tsotetsi', 'manmay.mohanty@reverside.co.za', '0846860904',  '27/06', '27/06/2008');
 INSERT INTO employees (id, name, surname, email, mobile, dob, doj)
@@ -15,11 +15,8 @@ VALUES (90,  'Mariaan', 'Deklerk', 'manmay.mohanty@reverside.co.za', '0846860904
 INSERT INTO employees (id, name, surname, email, mobile, dob, doj)
 VALUES (94,  'Ardhendu', 'Patri', 'manmay.mohanty@reverside.co.za', '0846860904',  '27/06', '27/06/2008');
 INSERT INTO employees (id, name, surname, email, mobile, dob, doj)
-VALUES (107, 'Manmay', 'Mohanty', 'manmay.mohanty@reverside.co.za', '0846860904',  '27/06', '27/06/2008');
+VALUES (107, 'Manmay', 'Mohanty', 'manmay.mohanty@reverside.co.za', '0846860904',  '30/06', '27/06/2008');
 INSERT INTO employees (id, name, surname, email, mobile, dob, doj)
 VALUES (111, 'Violet', 'Rikhotso', 'manmay.mohanty@reverside.co.za', '0846860904',  '27/06', '27/06/2008');
 INSERT INTO employees (id, name, surname, email, mobile, dob, doj)
 VALUES (116, 'Gayatri', 'Saripalli', 'manmay.mohanty@reverside.co.za', '0846860904',  '27/06', '27/06/2008');
-INSERT INTO employees (id, name, surname, email, mobile, dob, doj)
-VALUES (116, 'Gayatri', 'Saripalli', 'manmay.mohanty@reverside.co.za', '0846860904',  '27/06', '27/06/2008');
-
